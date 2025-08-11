4cf6154c08474af57b3385207d20bfe0a851bd50
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-08-11 14:09:52 +0530

    mergeSort practise 10

---
+++ Sorting Algorithm/MergeSort.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+public class MergeSort {
+
+
+
+
+
+	public static void main(String... args)
+	{
+		int[] intArr = {-3,-2,2,45,6,-35};
+		mergeSort(intArr,0,intArr.length);
+		System.out.println(Arrays.toString(intArr));
+
+	}
+
+	public static void mergeSort(int[] arr,int start,int end)
+	{
+		if(end - start < 2)
+		{
+			return;
+		}
+
+		int mid = (start + end) / 2 ;
+
+		mergeSort(arr,start,mid);
+		mergeSort(arr,mid,end);
+		merge(arr,start,mid,end);
+	}
+
+	public static void merge(int[] arr,int start,int mid,int end)
+	{
+		if(arr[mid-1] <= arr[mid])
+		{
+			return;
+		}
+
+		int i = start;
+		int j = mid;
+
+		int[] tempArr = new int[end-start+1];
+		int counter=0;
+
+		while ( i < mid && j < end)
+		{
+			tempArr[counter++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
+		}
+		if(i < mid)
+		{
+			System.arraycopy(arr,i,arr,start+counter,mid-i);
+		}
+
+		System.arraycopy(tempArr,0,arr,start,counter);
+
+	}
+
+
+
+}
