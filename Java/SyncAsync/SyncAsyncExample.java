edf8f7156b0e7ecf0bafd31d2d3ce99aebe9df15
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-08-25 13:48:23 +0530

    SyncAsync Example code

---
+++ Java/SyncAsync/SyncAsyncExample.java
@@ -0,0 +1,37 @@
+package SyncAsync;
+
+import java.nio.file.Path;
+
+public class SyncAsyncExample {
+
+
+
+	public static void main(String... args)
+	{
+
+
+		//Sync + blocking example
+		try(BufferedInputStream)
+
+
+
+	}
+
+
+	public static boolean helper(String str)
+	{
+		Path p = Path.of("./SyncAsync/dataFile")
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+}
